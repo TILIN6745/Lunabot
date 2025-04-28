617173e5be78ba6837ffe25ccd7f5db28abb7841
Tilin <jm293229@gmail.com>
2025-04-27 20:35:32 -0600

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,16 @@
+global.owner = ['+528336105471'] // Tu número de dueño
+global.botname = 'LunaBot Oficial'
+global.ownername = 'Tilin Ventas'
+global.instagram = 'https://instagram.com/tilin.ff.23'
+global.canalwhatsapp = 'https://whatsapp.com/channel/0029VauK3kA4SpkPQyez1z00'
+
+global.welcomeMessage = (userName, groupName, description) => {
+    return `¡Bienvenido/a ${userName} a *${groupName}*!
+    
+📄 Descripción: ${description}
+
+Soy *LunaBot*, creada por Tilin Ventas.
+Disfruta tu estancia en el grupo. ¡Activa notificaciones!`
+}
+
+global.logo = './media/logo.jpg' // Ruta del logo para cuando se use el comando .logo
